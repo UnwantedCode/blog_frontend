b7840db331a0e234804d12803bd6737847416dbb
Adam Tarkowski <adam@dharsa.com>
2024-09-13 11:25:04 +0200

    details

--- src/Components/MainPageSlider/MainPageSlider.jsx
+++ src/Components/MainPageSlider/MainPageSlider.jsx
@@ -13,6 +13,7 @@ function MainPageSlider() {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        // centerMode: true,
     };
 
     return (
--- src/Containers/TestPage/TestPage.jsx
+++ src/Containers/TestPage/TestPage.jsx
@@ -1,32 +1,26 @@
 import {Helmet} from "react-helmet-async";
-import styles from './MainPage.module.css';
-import ArticleItem from "../../Components/ArticleItem/ArticleItem.jsx";
+import styles from './TextPage.module.css';
 import RightPanel from "../../Components/RightPanel/RightPanel.jsx";
-import MainPageSlider from "../../Components/MainPageSlider/MainPageSlider.jsx";
 
 //show big image and some text under it
-function MainPage() {
+function TextPage() {
     return (
         <>
             <Helmet>
                 <title>Strona glówna</title>
             </Helmet>
-            {/*<MainPageSlider />*/}
-
             <div className={styles.container}>
                 <div className={styles.mainLeft}>
-                    <ArticleItem/>
-                    <ArticleItem/>
-                    <ArticleItem/>
-                    <ArticleItem/>
+
 
                 </div>
                 <RightPanel/>
             </div>
 
 
+            {/*<MainPageSlider />*/}
         </>
     );
 }
 
-export default MainPage;
+export default TextPage;
